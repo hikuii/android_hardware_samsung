42329cff10bee6f75c49cb86138c3477fefed710
leewp14 <lee.wp14@gmail.com>
2020-05-10 00:34:11 +0800

    Revert "WIP: exynos: SEC_OMX_H264dec.c solve dump when decoding hd and fullhd h264"
    This reverts commit f1b9dcdf726775d1a2f9673dca726ab3381af2a2.

--- exynos/multimedia/openmax/component/video/dec/h264/SEC_OMX_H264dec.c
+++ exynos/multimedia/openmax/component/video/dec/h264/SEC_OMX_H264dec.c
@@ -55,7 +55,7 @@
 #define SEC_LOG_OFF
 #include "SEC_OSAL_Log.h"
 
-#define H264_DEC_NUM_OF_EXTRA_BUFFERS 1
+#define H264_DEC_NUM_OF_EXTRA_BUFFERS 7
 
 #ifdef S3D_SUPPORT
 #define ADD_SPS_PPS_I_FRAME
