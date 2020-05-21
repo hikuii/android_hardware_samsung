c6263e2efca102ec94ab2f744f5c9cf0a0b83348
leewp14 <lee.wp14@gmail.com>
2020-05-22 00:20:00 +0800

    samsung: lineage specifics removal

--- Android.mk
+++ Android.mk
@@ -41,7 +41,6 @@ include $(SAM_ROOT)/loki_tool/Android.mk
 endif
 
 ifeq ($(BOARD_VENDOR),samsung)
-include $(SAM_ROOT)/AdvancedDisplay/Android.mk
 include $(SAM_ROOT)/audio/Android.mk
 include $(SAM_ROOT)/consumerir/Android.mk
 include $(SAM_ROOT)/dtbhtool/Android.mk
@@ -49,6 +48,5 @@ include $(SAM_ROOT)/fingerprint/Android.mk
 include $(SAM_ROOT)/hidl/Android.mk
 include $(SAM_ROOT)/liblights/Android.mk
 include $(SAM_ROOT)/modemloader/Android.mk
-include $(SAM_ROOT)/power/Android.mk
 include $(SAM_ROOT)/ril/Android.mk
 endif
